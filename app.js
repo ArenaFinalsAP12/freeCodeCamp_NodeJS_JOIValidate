07d7c9547dd4d5075ec1c13940758cbf1378a267
Atsushi <atsushi.idobe86@gmail.com>
2024-04-11 14:19:33 -0400

    This is the new branch that contains the code in the following section "User Input Validation with JOI Nested Objects and Arrays".

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+// User Input Validation with Express and JOI
+/*
 const express = require('express');
 const path = require('path');
 const Joi = require('joi');
@@ -28,3 +30,39 @@ app.post('/',(req,res)=> {
 });
 
 app.listen(3000);
+*/
+// User Input Validation with JOI Validating Nested Objects and Arrays (part 2)
+
+const Joi = require('joi'); 
+
+const arrayString = ['banana','bacon','cheese'];
+const arrayObjects = [{example: 'example1'},{example: 'example2'},{example: 'example3'}];
+
+const userInput = { personalInfo: {
+                        streetAddress: '123987987',
+                        city: 'kljlkajd',
+                        state: 'fl'
+                    },
+                    preferences: arrayObjects};
+
+const personalInfoSchema = Joi.object().keys({
+    streetAddress : Joi.string().trim().required(),
+    city : Joi.string().trim().required(),
+    state : Joi.string().trim().length(2).required(),
+});
+
+const preferencesSchema = Joi.array().items(Joi.object().keys({
+    example: Joi.string().required()
+})); // This ensures that all items in the array are strings
+
+const schema = Joi.object().keys({
+    personalInfo: personalInfoSchema,
+    preferences: preferencesSchema
+});
+
+Joi.validate(userInput,schema,(err,result)=> {
+    if (err)
+        console.log(err)
+    else
+        console.log(result);
+});
